751461e46e3fce12e37a7de9e428a03a7c185edf
Piyush <bhattpiyush03@gmail.com>
2023-12-20 13:21:59 +0530

    Removed chat_data.txt

--- nebula.py
+++ nebula.py
@@ -4,15 +4,15 @@ import time
 from firebase_admin import credentials, db
 import firebase_admin
 
-no_of_bots = 10
-delay=2
-no_of_msg_per_bot = 5
-url = 'https://randomuser.me/api/'
+no_of_bots = 10 #set the number of bots you want to generate
+delay=2  #the delay of bot msg generation
+no_of_msg_per_bot = 5 #how many msg you want to assign to a bot
+url = 'https://randomuser.me/api/' #you can use any other site for random name generation but then change the code accordingly
 sentences = []
-gender=["male","female","Not Confirmed","Non Binary"]
-cred = credentials.Certificate('tracki-5f30e-firebase-adminsdk-lad77-afad164095.json')  
+gender=["male","female","Not Confirmed","Non Binary"] #list of genders you are using or you can use the site generated genders also
+cred = credentials.Certificate('tracki-5f30e-firebase-adminsdk-lad77-afad164095.json')  #your credentials in case you are using firebase
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://tracki-5f30e-default-rtdb.firebaseio.com/' 
+    'databaseURL': 'your_database_url_here'  #database url
 })
 
 # Function to print messages
@@ -25,11 +25,11 @@ def new_msg(bots):
              'age':str(bot['age']),
              'uid':bot['uid']}
         print("newmsg",msg)
-        path=db.reference('home')
+        path=db.reference('home') #heres the refrence to the databse in which we are pusing data
         path.push().set(msg)
         time.sleep(delay)
     
-
+#function to generate bots
 def gen_bot():
     global sentences
     global no_of_bots
@@ -60,7 +60,6 @@ def gen_bot():
         random.shuffle(bots)
         for bot in bots:
             random.shuffle(bot['message'])
-        
         new_msg(bots)
         
     else:
